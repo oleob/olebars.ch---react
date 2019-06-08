be1939c9d9c8338c0d7f2df5bcc00543a4da6181
Ole Barsch <ole.barsch@gmail.com>
2019-06-09 01:31:07 +0200

    fix link

--- src/containers/Projects/projectList.js
+++ src/containers/Projects/projectList.js
@@ -24,7 +24,7 @@ const externalProjects = [
     title: 'Old homepage',
     text: 'Deprecated version of this page. Contains a cool menu.',
     link: 'https://practical-bohr-7f94dd.netlify.com/',
-    source: 'https://github.com/oleob/olebars.ch',
+    source: 'https://github.com/oleob/olebars.ch-old',
     tags: ['React', 'CSS3']
   },
   {
