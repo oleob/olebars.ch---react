df91c9bf8af7902f006971a71014bfa09900a4bc
Ole Barsch <ole.barsch@gmail.com>
2019-06-10 22:57:57 +0200

    update pagelink css

--- src/components/PageLinks/PageLinks.jsx
+++ src/components/PageLinks/PageLinks.jsx
@@ -5,11 +5,11 @@ import './page-links.scss';
 
 const PageLinks = () => (
   <div className="page-links">
-    <Link to="/projects" className="page-links__link page-links__link--left">
+    <Link to="/projects" className="paper page-links__link page-links__link--left">
       <i className="page-links__icon fas fa-code" />
       <p>Projects</p>
     </Link>
-    <Link to="/about" className="page-links__link page-links__link--right">
+    <Link to="/about" className="paper page-links__link page-links__link--right">
       <i className="page-links__icon fas fa-info-circle" />
       <p>About</p>
     </Link>
